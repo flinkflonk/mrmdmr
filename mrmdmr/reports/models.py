cf063287cd527421314552d7741edc31600769f7
Michael Hinz <michael@hinz.no>
2020-07-15 17:43:53 +0200

    Add the central model and a basic script. Doesn't do anything yet.

---
+++ mrmdmr/reports/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Report():
+    
+    def __init__(self):
+        super().__init__()
---
+++ mrmdmr/scripts/read_from_mailbox.py
@@ -0,0 +1,9 @@
+from reports.models import Report
+
+"""
+See https://django-extensions.readthedocs.io/en/latest/runscript.html
+"""
+
+def run():
+    # do nothing for now
+    pass
